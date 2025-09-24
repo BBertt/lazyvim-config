3977c340e854dc5a1f2316e369fa0c02d6f46c0f
Bertrand <bertrandwijaya05@gmail.com>
2025-09-24 21:28:49 +0700

    :sparkles: Enabled treesitter blade plugin

--- lua/plugins/treesitter-blade.lua
+++ lua/plugins/treesitter-blade.lua
@@ -1,32 +1,32 @@
--- -- lua/plugins/treesitter-blade.lua
--- return {
---   {
---     "nvim-treesitter/nvim-treesitter",
---     opts = function(_, opts)
---       vim.list_extend(opts.ensure_installed, {
---         "blade",
---         "php_only",
---       })
---
---       opts.parser_configs = opts.parser_configs or {}
---       opts.parser_configs.blade = {
---         install_info = {
---           url = "https://github.com/EmranMR/tree-sitter-blade",
---           files = { "src/parser.c" },
---           branch = "main",
---         },
---         filetype = "blade",
---       }
---     end,
---     config = function(_, opts)
---       -- The line below was causing the crash and should be removed.
---       -- vim.api.nvim_treesitter_start()
---
---       vim.filetype.add({
---         pattern = {
---           [".*%.blade%.php"] = "blade",
---         },
---       })
---     end,
---   },
--- }
+-- lua/plugins/treesitter-blade.lua
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "blade",
+        "php_only",
+      })
+
+      opts.parser_configs = opts.parser_configs or {}
+      opts.parser_configs.blade = {
+        install_info = {
+          url = "https://github.com/EmranMR/tree-sitter-blade",
+          files = { "src/parser.c" },
+          branch = "main",
+        },
+        filetype = "blade",
+      }
+    end,
+    config = function(_, opts)
+      -- The line below was causing the crash and should be removed.
+      -- vim.api.nvim_treesitter_start()
+
+      vim.filetype.add({
+        pattern = {
+          [".*%.blade%.php"] = "blade",
+        },
+      })
+    end,
+  },
+}
