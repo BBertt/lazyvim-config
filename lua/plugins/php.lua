b9822512d1432e50d0f77020af3916f859d24298
Bertrand <bertrandwijaya05@gmail.com>
2025-10-01 22:28:16 +0700

    :bug: php only use intelephense

--- lua/plugins/php.lua
+++ lua/plugins/php.lua
@@ -1,5 +1,14 @@
 return {
   {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        phpactor = false,
+        intelephense = {},
+      },
+    },
+  },
+  {
     -- Set Laravel Pint as the default PHP formatter with PHP CS Fixer as a fall back.
     "stevearc/conform.nvim",
     optional = true,
