311768567cf1f18ad194a18d6a748eb7c8fd6ed4
Bertrand <bertrand.laptop@dewaweb.com>
2025-10-22 10:33:59 +0700

    ✨ New CMP and diffview

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,8 @@
+-- Example for lazy.nvim
+return {
+  "sindrets/diffview.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" }, -- Required dependency
+  config = function()
+    require("diffview").setup()
+  end,
+}
