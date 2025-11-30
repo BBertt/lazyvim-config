0e412aa54910c09471c16385eb4ad300609c91a6
Bertrand Valentino Wijaya <bertrandwijaya05@gmail.com>
2025-11-30 11:27:07 +0700

    added borders

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,10 +1,43 @@
 return {
-  "folke/noice.nvim",
-  opts = {
-    lsp = {
-      hover = {
-        silent = true,
+  -- 1. Configure Noice (Your current setup + extras)
+  {
+    "folke/noice.nvim",
+    opts = {
+      lsp = {
+        hover = {
+          silent = true,
+        },
+      },
+      presets = {
+        lsp_doc_border = true, -- Adds border to Hover/Signature
+        command_palette = true, -- Adds border to the cmdline
       },
     },
+  },
+
+  -- 2. Configure Diagnostics (Floating window borders)
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      -- Ensure the 'diagnostics' table exists
+      opts.diagnostics = opts.diagnostics or {}
+      opts.diagnostics.float = opts.diagnostics.float or {}
+
+      -- Add the border to the diagnostic float
+      opts.diagnostics.float.border = "rounded"
+    end,
+  },
+
+  -- 3. Configure Suggestions/Completion (nvim-cmp borders)
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.window = opts.window or {}
+
+      -- Add borders to the completion menu and documentation
+      opts.window.completion = cmp.config.window.bordered()
+      opts.window.documentation = cmp.config.window.bordered()
+    end,
   },
 }
