cb8b95a01e1c207b60361c5cccf1aad2347a074b
Bertrand <bertrand@dewaweb.com>
2026-02-06 08:02:01 +0700

    ✨ update neo-tree to be able to follow directory changes

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,5 +1,14 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  filesystem = {
+    -- This ensures that when you change the root in Neo-tree,
+    -- it also changes the actual 'cd' of your Neovim session.
+    bind_to_cwd = true,
+    follow_current_file = {
+      enabled = true,
+    },
+    use_libuv_file_watcher = true, -- Auto-updates when files change
+  },
   opts = {
     window = {
       position = "right",
